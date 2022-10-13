8e776fd12f721dac2d02a72912636d8da0f34898
Yahya <yahyaua5@gmail.com>
2022-10-13 09:08:35 +0100

    If approved 10-POST /api/articles/:article_id/comments complete

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,44 +1,71 @@
-const { fetchTopics, fetchArticleByID, fetchUsers, fetchAndPatchArticleByID, fetchArticles} = require("../models/model");
+const {fetchTopics, fetchUsers, fetchArticles ,fetchArticleByID, fetchCommentsFromArticleByID, fetchAndPatchArticleByID, fetchAndPostCommentsByArticleID, } = require("../models/model");
+
 
 exports.getTopics = (req, res, next) => {
-    fetchTopics()
+  fetchTopics()
     .then((topics) => {
-        res.status(200).send({ topics });
+      res.status(200).send({ topics });
   })
   .catch(next)
 };
 
-exports.getArticleByID = (req, res, next) => {
-    const {article_id} = req.params
-    fetchArticleByID(article_id)
-    .then((article) => {
-        res.status(200).send({ article });
-  })
-  .catch(next)
-};
 
 exports.getUsers = (req, res, next) => {
-    fetchUsers()
+  fetchUsers()
     .then((users) => {
-        res.status(200).send({ users });
-  })
+      res.status(200).send({ users });
+   })
   .catch(next)
 };
+
+
+exports.getArticles = (req, res, next) => {
+  fetchArticles()
+    .then((article) => {
+      res.status(200).send({ article });
+   })
+  .catch(next)
+};
+
+
+exports.getArticleByID = (req, res, next) => {
+  const {article_id} = req.params
+  fetchArticleByID(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+   })
+  .catch(next)
+};
+
+
+exports.getCommentsFromArticleByID = (req, res, next) => {
+  const {article_id} = req.params
+  fetchCommentsFromArticleByID(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+   })
+  .catch(next)
+};
+
 
 exports.patchArticleByID = (req, res, next) => {
   const {article_id} = req.params
   const {inc_votes} = req.body
   fetchAndPatchArticleByID(article_id, inc_votes)
-  .then((article) => {
+    .then((article) => {
       res.status(200).send({ article });
-})
-.catch(next)
+   })
+  .catch(next)
 };
 
-exports.getArticles = (req, res, next) => {
-  fetchArticles()
-  .then((article) => {
-      res.status(200).send({ article });
-})
-.catch(next)
+
+exports.postCommentsByArticleID = (req, res, next) => {
+  const {article_id} = req.params
+  const username = req.body.username
+  const body = req.body.body
+  fetchAndPostCommentsByArticleID(article_id, username, body)
+    .then((article) => {
+      res.status(201).send({ article });
+   })
+  .catch(next)
 };
--- models/model.js
+++ models/model.js
@@ -1,61 +1,93 @@
 const db = require("../db/connection");
 
+
 exports.fetchTopics = () => {  
   return db
-    .query(
-        `SELECT * FROM topics`     
-        )
+    .query(`SELECT * FROM topics`)
     .then(({ rows }) => {
-        if (rows.length == 0) {
-            return Promise.reject({status: 404, msg: "This data does not exist"})
-         }
-         else { 
-      return rows;
-         }
+      if (rows.length == 0) {
+        return Promise.reject({status: 404, msg: "This data does not exist"})
+      }
+      else { return rows;}
     })
-    
 };
 
+
+
+exports.fetchUsers = () => {  
+  return db
+    .query(`SELECT * FROM users`)
+    .then(({ rows }) => {
+      if (rows.length == 0) {
+          return Promise.reject({status: 404, msg: "This data does not exist"})
+      }
+      else {return rows}  
+    })
+};
+
+
+
+exports.fetchArticles = () => {  
+  return db
+    .query(`
+    SELECT DISTINCT articles.*, COUNT (comments.comment_id) ::INT AS comment_count 
+    FROM articles
+    JOIN comments ON comments.article_id = articles.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC`
+    )
+      .then(({ rows }) => {
+        if (rows.length == 0) {
+          return Promise.reject({status: 404, msg: "This article does not exist"})
+        }
+        else {return rows}  
+      }) 
+  };
+
+
+
 exports.fetchArticleByID = (article_id) => {  
-    return db
-    .query( `
+  return db
+    .query(`
     SELECT articles.*, COUNT (comments.comment_id) AS comment_count 
     FROM articles
     JOIN comments ON comments.article_id = articles.article_id
     WHERE articles.article_id = $1
     GROUP BY articles.article_id`, [article_id]
-    
     )
       .then(({ rows }) => {
         if (rows.length == 0) {
-           return Promise.reject({status: 404, msg: "This article does not exist"})
+          return Promise.reject({status: 404, msg: "This article does not exist"})
         }
-        else {
-        return rows[0];
-        }  
-    })
-      
-  };
+        else {return rows[0]}  
+      }) 
+};
 
 
-  exports.fetchUsers = () => {  
-    return db
-      .query(
-          `SELECT * FROM users`     
-          )
+
+exports.fetchCommentsFromArticleByID = (article_id) => {  
+  return db
+    .query(
+    `SELECT comments.comment_id, comments.body, comments.author, comments.created_at, comments.votes
+    FROM comments
+    JOIN articles
+    ON comments.article_id = articles.article_id
+    WHERE comments.article_id = $1
+    ORDER BY created_at DESC`, [article_id]
+    )
+   
       .then(({ rows }) => {
         if (rows.length == 0) {
-            return Promise.reject({status: 404, msg: "This data does not exist"})
-         }
-        else { 
-        return rows;
-        }  
-    })
-      
-  };
+          return Promise.reject({status: 404, msg: "This article does not exist"})
+        }
+        else {return rows}  
+      }) 
+};
 
-  exports.fetchAndPatchArticleByID = (article_id, inc_votes) => {
-    return db
+
+
+exports.fetchAndPatchArticleByID = (article_id, inc_votes) => {
+  return db
     .query(`
        UPDATE articles 
        SET votes = votes + $2
@@ -66,30 +98,26 @@ exports.fetchArticleByID = (article_id) => {
         if (rows.length == 0) {
           return Promise.reject({status: 404, msg: "This data does not exist"})
        }
-      else { 
-      return rows[0];
-      }  
-    })
-      
-  };
+      else {return rows[0]}  
+    })  
+};
 
-  exports.fetchArticles = () => {  
-    return db
-    .query( `
-    SELECT DISTINCT articles.*, COUNT (comments.comment_id) ::INT AS comment_count 
-    FROM articles
-    JOIN comments ON comments.article_id = articles.article_id
-    GROUP BY articles.article_id
-    ORDER BY articles.created_at DESC
-    `
+
+
+exports.fetchAndPostCommentsByArticleID = (article_id, username, body) => {
+  if (!(article_id && username && body)) {
+    return Promise.reject({status: 400, msg: "Bad Request"})
+  }
+
+  return db
+    .query(
+      `INSERT INTO comments (article_id, author , body)
+      VALUES($1,$2,$3) RETURNING*;` , [article_id, username, body]
     )
-      .then(({ rows }) => {
+      .then(({rows} ) => {
         if (rows.length == 0) {
-           return Promise.reject({status: 404, msg: "This article does not exist"})
+          return Promise.reject({status: 404, msg: "This article does not exist"})
         }
-        else {
-        return rows
-        }  
-    })
-      
-  };
+        else {return rows[0]}  
+      }) 
+};
